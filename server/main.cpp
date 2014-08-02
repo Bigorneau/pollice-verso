d6db1ff482ccea9056ceaac2d0eeaee2008d5034
Patrice Blin <dev@patriceblin.com>
2014-08-03 00:57:24 +0200

    server: multiples clients

--- server/main.cpp
+++ server/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include <list>
 
 #include <SFML/Network.hpp>
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::list;
 
 class Server
 {
@@ -14,8 +16,12 @@ public:
 	~Server();
 	int run();
 private:
+	int acceptClient(sf::TcpListener &listener);
+	int receivePacket(sf::TcpSocket &client);
+	//
 	unsigned short m_port;
-	sf::TcpSocket m_socket;
+	sf::SocketSelector m_selector;
+	list<sf::TcpSocket*> m_clients;
 };
 
 Server::Server(unsigned short port) :
@@ -27,26 +33,62 @@ Server::~Server()
 {
 }
 
+int Server::acceptClient(sf::TcpListener &listener)
+{
+	sf::TcpSocket *client = new sf::TcpSocket();
+	if (listener.accept(*client) != sf::Socket::Done) {
+		delete client;
+		return -1;
+	}
+	m_clients.push_back(client);
+	m_selector.add(*client);
+	cout << "--- new client ";
+	cout << client->getRemoteAddress();
+	cout << ":" << client->getRemotePort();
+	cout << " (local: " << client->getLocalPort() << ")" << endl;
+	return 0;
+}
+
 int Server::run()
 {
 	sf::TcpListener listener;
 	if (listener.listen(m_port) != sf::Socket::Done)
 		return -1;
-	if (listener.accept(m_socket) != sf::Socket::Done)
-		return -1;
-	cout << "client: " << m_socket.getRemoteAddress();
-	cout << ":" << m_socket.getRemotePort();
-
-	sf::Packet packet;
-	string message;
+	m_selector.add(listener);
 	while (true) {
-		m_socket.receive(packet);
-		if (packet >> message)
-			cout << message << endl;
-		packet.clear();
-		message.clear();
+		if (!m_selector.wait())
+			continue;
+
+		if (m_selector.isReady(listener)) {
+			acceptClient(listener);
+		}
+		else {
+			list<sf::TcpSocket*>::iterator it;
+			for (it = m_clients.begin(); it != m_clients.end(); it++) {
+				sf::TcpSocket &client = **it;
+				if (m_selector.isReady(client))
+					receivePacket(client);
+			}
+		}
 		sf::sleep(sf::milliseconds(10));
 	}
+	return 0;
+}
+
+int Server::receivePacket(sf::TcpSocket &client)
+{
+	sf::Packet packet;
+	string message;
+	client.receive(packet);
+	if (packet >> message) {
+		cout << "client: " << client.getRemoteAddress();
+		cout << ":" << client.getRemotePort();
+		cout << " (local: " << client.getLocalPort() << ")";
+		cout << " message:" << message << endl;
+	}
+	packet.clear();
+	message.clear();
+	sf::sleep(sf::milliseconds(10));
 
 	return 0;
 }
